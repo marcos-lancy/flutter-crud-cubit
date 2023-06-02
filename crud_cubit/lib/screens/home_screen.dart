3a31a30cc1e654e45155a0beb8daf2e75d2b7330
marcos-lancy <marcos.system32@gmail.com>
2023-06-02 19:48:44 -0300

    correcao diretorios

--- crud_cubit/lib/screens/home_screen.dart
+++ crud_cubit/lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
-import 'package:crud_cubit/cubit/categoria_cubit.dart';
-import 'package:crud_cubit/cubit/categoria_state.dart';
 import 'package:crud_cubit/screens/adicionar_screen.dart';
+import 'package:crud_cubit/screens/cubit/categoria_cubit.dart';
+import 'package:crud_cubit/screens/cubit/categoria_state.dart';
 import 'package:crud_cubit/services/categoria_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
